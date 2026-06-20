6ee0a1803fdcfc83a7e069549cbc30b34ffa1339
Hassan Nass <hassannass613@gmail.com>
2026-06-20 13:53:33 +0300

    set up React Router with Home and RecipeDetails routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home.jsx'
+import RecipeDetails from './RecipeDetails.jsx'
 
 function App() {
 
   return (
     <div className="container">
       <h1 className='header'>Recipe Browser</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/RecipeDetails.jsx
@@ -0,0 +1,8 @@
+
+function RecipeDetails() {
+	return (
+		<h2>Recipe Details Page</h2>
+	)
+}
+
+export default RecipeDetails

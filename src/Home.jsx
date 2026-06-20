84f2dac7cbde9b2b009b3342bcee489438e49431
Hassan Nass <hassannass613@gmail.com>
2026-06-20 18:10:51 +0300

    add active state for category filter buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,10 +16,35 @@ function Home() {
 	return (
 		<div className="home-container">
 			<h1>Home</h1>
-			<button className="btn-all" onClick={() => handleCategoryChange("All")}>All</button>
-			<button className="btn-breakfast" onClick={() => handleCategoryChange("Breakfast")}>Breakfast</button>
-			<button className="btn-lunch" onClick={() => handleCategoryChange("Lunch")}>Lunch</button>
-			<button className="btn-dessert" onClick={() => handleCategoryChange("Dessert")}>Dessert</button>
+
+			<button 
+				className={`btn-all ${selectedCategory === "All" ? "active" : ""}`} 
+				onClick={() => handleCategoryChange("All")}
+			>
+				All
+			</button>
+
+			<button 
+				className={`btn-breakfast ${selectedCategory === "Breakfast" ? "active" : ""}`} 
+				onClick={() => handleCategoryChange("Breakfast")}
+			>
+				Breakfast
+			</button>
+			
+			<button 
+				className={`btn-lunch ${selectedCategory === "Lunch" ? "active" : ""}`}
+				onClick={() => handleCategoryChange("Lunch")}
+			>
+				Lunch
+			</button>
+
+			<button 
+				className={`btn-dessert ${selectedCategory === "Dessert" ? "active" : ""}`} 
+				onClick={() => handleCategoryChange("Dessert")}
+			>
+				Dessert
+			</button>
+
 			<ul>
 				{filteredRecipes.map((recipe, id) => (
 					<RecipeCard recipe={recipe} key={recipe.id} />

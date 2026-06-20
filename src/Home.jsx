5706dc0773b07d556c8502b2d0b71a0e03137f61
Hassan Nass <hassannass613@gmail.com>
2026-06-20 15:45:30 +0300

    add category filter functionality

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,27 @@
 import recipes from "./recipes"
 import RecipeCard from "./RecipeCard"
+import { useState } from "react"
 
 function Home() {
+	const [selectedCategory, setSelectedCategory] = useState("All");
+
+	function handleCategoryChange(category) {
+		setSelectedCategory(category)
+	}
+
+	const filteredRecipes = selectedCategory === "All"
+		? recipes
+		: recipes.filter(recipe => recipe.category === selectedCategory)
+
 	return (
 		<div className="home-container">
 			<h1>Home</h1>
+			<button className="btn-all" onClick={() => handleCategoryChange("All")}>All</button>
+			<button className="btn-breakfast" onClick={() => handleCategoryChange("Breakfast")}>Breakfast</button>
+			<button className="btn-lunch" onClick={() => handleCategoryChange("Lunch")}>Lunch</button>
+			<button className="btn-dessert" onClick={() => handleCategoryChange("Dessert")}>Dessert</button>
 			<ul>
-				{recipes.map((recipe, id) => (
+				{filteredRecipes.map((recipe, id) => (
 					<RecipeCard recipe={recipe} key={recipe.id} />
 				))}
 			</ul>
